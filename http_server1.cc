71e2ca5ce54762a2095805e471e20b54b7d45f23
Brendan Lestander <blestander@pitt.edu>
2017-09-22 12:15:11 -0400

    Added basic connection capability to server, to be tested in lab. Does not yet process requests.

--- http_server1.cc
+++ http_server1.cc
@@ -13,6 +13,10 @@ int main(int argc, char * argv[]) {
     int server_port = -1;
     int rc          =  0;
     int sock        = -1;
+    int connect     = -1;
+    struct addrinfo hints, *res;
+    struct sockaddr clientaddr;
+    socklen_t addrlen;
 
     /* parse command line args */
     if (argc != 3) {
@@ -27,19 +31,43 @@ int main(int argc, char * argv[]) {
 	exit(-1);
     }
 
+    /* get address info */
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    minet_getaddrinfo(NULL, server_port, &hints, &res);
+
     /* initialize and make socket */
 
-    /* set server address*/
+    sock = minet_socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sock < 0) {
+        fprintf(stderr, "Unable to initialize socket.");
+        exit(-1);
+    }
 
+    /* set server address*/
     /* bind listening socket */
 
+    minet_bind(sock, res->ai_addr, res->addr_len);
+
     /* start listening */
+
+    minet_listen(sock, 20);
 
     /* connection handling loop: wait to accept connection */
 
     while (1) {
-	/* handle connections */
-	rc = handle_connection(sock);
+	    /* handle connections */
+        connect = minet_accept(sock);
+        if (connect < 0)
+            continue;
+
+        fprintf(stderr, "Connected\n");
+        //rc = handle_connection(sock);
+        
+        minet_close(connect);
     }
 }
 
@@ -49,15 +77,15 @@ int handle_connection(int sock) {
     const char * ok_response_f = "HTTP/1.0 200 OK\r\n"	\
 	"Content-type: text/plain\r\n"			\
 	"Content-length: %d \r\n\r\n";
- 
+
     const char * notok_response = "HTTP/1.0 404 FILE NOT FOUND\r\n"	\
 	"Content-type: text/html\r\n\r\n"			\
 	"<html><body bgColor=black text=white>\n"		\
 	"<h2>404 FILE NOT FOUND</h2>\n"
 	"</body></html>\n";
-    
+
     /* first read loop -- get request and headers*/
-    
+
     /* parse request to get file name */
     /* Assumption: this is a GET request and filename contains no spaces*/
 
@@ -66,15 +94,15 @@ int handle_connection(int sock) {
     /* send response */
     if (ok) {
 	/* send headers */
-	
+
 	/* send file */
-	
+
     } else {
 	// send error response
     }
-    
+
     /* close socket and free space */
-  
+
     if (ok) {
 	return 0;
     } else {
