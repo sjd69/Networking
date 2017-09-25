e7dcf1378cf9bdf301554c875265e2d0d51d49b0
Brendan Lestander <blestander@pitt.edu>
2017-09-25 09:14:10 -0400

    HTTP_Server2 should mostly work, though a few more test cases couldn't hurt

--- http_server2.cc
+++ http_server2.cc
@@ -6,9 +6,9 @@
 
 #define BUFSIZE 1024
 #define FILENAMESIZE 100
-#define CONNECTION_INIT 16;
+#define CONNECTIONS_INIT 16
 
-int handle_connection(int sock);
+int handle_connection(int sock, char* buffer);
 void setConnections(fd_set* set, int* connections, int* count);
 void addConnection(int connect, int** connections, int* count, int* max);
 void removeConnection(int connect, int* connections, int* count);
@@ -22,11 +22,12 @@ int main(int argc, char * argv[]) {
     struct sockaddr_in clientaddr;
     int i;
     int* connections;
-    int connectionCount = 0;
-    int connectionsMax = CONNECTION_INIT;
+    int connectionsCount = 0;
+    int connectionsMax = CONNECTIONS_INIT;
     socklen_t addrlen;
     fd_set set;
     char buffer[BUFSIZE];
+    struct timeval timeout;
 
     /* parse command line args */
     if (argc != 3) {
@@ -76,19 +77,22 @@ int main(int argc, char * argv[]) {
     minet_listen(sock, 20);
 
     /* connection handling loop: wait to accept connection */
-    connections = malloc(sizeof int * CONNECTIONS_INIT);
+    connections = (int*) malloc(sizeof(int) * CONNECTIONS_INIT);
+
+    timeout.tv_sec  = 1;
+    timeout.tv_usec = 0;
 
     while (1) {
 
 	    /* create read list */
 
-        FD_ZERO(set);
-        FD_SET(sock);
-        setConnections(&set, connections, *connectionsCount);
+        FD_ZERO(&set);
+        FD_SET(sock, &set);
+        setConnections(&set, connections, &connectionsCount);
 
 	    /* do a select */
 
-        int ready = minet_select(socketMax, &set, NULL, &aSet, NULL);
+        int ready = minet_select(socketMax, &set, NULL, NULL, &timeout);
 
 	    /* process sockets that are ready */
 
@@ -98,22 +102,25 @@ int main(int argc, char * argv[]) {
             int connect = minet_accept(sock, &clientaddr);
             if (connect >= socketMax)
                 socketMax = connect + 1;
-            addConnection(connect, connections, *connectionsCount, *connectionsMax);
+            addConnection(connect, &connections, &connectionsCount, &connectionsMax);
+	    fprintf(stderr, "Connected socket %d\r\n", connect);
         }
 
 	    /* for a connection socket, handle the connection */
         for (i = 0; i < connectionsCount; i++) {
             if (FD_ISSET(connections[i], &set)) {
-	            rc = handle_connection(sock, buffer);
+	        fprintf(stderr, "Entered handling conditions for socket %d\r\n", connections[i]);        
+	            rc = handle_connection(connections[i], buffer);
                 removeConnection(connections[i], connections, &connectionsCount);
             }
         }
 
     }
+    fprintf(stderr, "Shutting down\r\n");
     free(connections);
 }
 
-int handle_connection(int sock) {
+int handle_connection(int sock, char* buffer) {
     bool ok = false;
 
     const char * ok_response_f = "HTTP/1.0 200 OK\r\n"	\
@@ -129,7 +136,7 @@ int handle_connection(int sock) {
     /* first read loop -- get request and headers*/
 
     int in = minet_read(sock, buffer, BUFSIZE);
-    fprintf(stderr, "Diagnostic Output:\r\n%s\r\n", buffer);
+    //fprintf(stderr, "Diagnostic Output:\r\n%s\r\n", buffer);
 
     /* parse request to get file name */
     /* Assumption: this is a GET request and filename contains no spaces*/
@@ -173,14 +180,14 @@ int handle_connection(int sock) {
 
 void setConnections(fd_set* set, int* connections, int* max) {
     int i;
-    for (i = 0; i < max; i++)
+    for (i = 0; i < *max; i++)
         FD_SET(connections[i], set);
 }
 
 void addConnection(int connect, int** connections, int* count, int* max) {
     if (*count == *max) {
-        *max = 2 * *max;
-        *connections = realloc(*connections, *max * sizeof int);
+        *max = 2 * (*max);
+        *connections = (int*)realloc(*connections, (*max) * sizeof(int));
     }
 
     *(*connections + *count) = connect;
@@ -190,7 +197,7 @@ void addConnection(int connect, int** connections, int* count, int* max) {
 void removeConnection(int connect, int* connections, int* count) {
     int i;
     for (i = 0; connections[i] != connect; i++);
-    for (i++, i < *count; i++)
-        connection[i - 1] = connections[i];
+    for (i++; i < *count; i++)
+        connections[i - 1] = connections[i];
     *count = *count - 1;
 }
